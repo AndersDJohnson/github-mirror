81d47a0cf5112f0d19ede345150e9febe9135268
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2019-05-28 20:42:04 -0500

    test: token from env

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 var mirror = require('./src')
-var token = require('./token.private.json').token
 
 mirror({
   reposFile: './data/repos.json',
   dir: './data/mirror',
   maxRepos: 2,
   // clone: false,
-  token: token
+  token: process.env.GITHUB_TOKEN
 })
